386d264f712ec932b936837bd24ce724a92198f0
Josh Woods <joshualwoods@gmail.com>
2016-02-09 22:13:05 -0500

    [Josh] added comments to the data input code

--- src/backend/Main.java
+++ src/backend/Main.java
@@ -7,6 +7,11 @@ import java.util.Scanner;
 
 import utils.FlexRedBlackTree;
 
+/**
+ * this is the main class that the program will run from
+ * @author woodsjl
+ *
+ */
 public class Main {
 	
 	private FlexRedBlackTree popTree;
@@ -20,11 +25,20 @@ public class Main {
 		}
 	}
 	
+	/**
+	 * the importFromTxtFileToTree() method will load raw data from a text file
+	 * and then import all of the data into a TopDownRedBlackTree for sorted 
+	 * data storage 
+	 * @throws IOException
+	 */
 	private static void importFromTxtFileToTree() throws IOException{
+		// import data from a file and store it in a file type
 		File inputFile = new File("src/backend/KansasCities.txt");
+		// create a scanner to scan through the newly created file
 		Scanner inScanner = new Scanner(inputFile);
 		ArrayList<String> cityName = new ArrayList<String>();
 		ArrayList<String> cityPop = new ArrayList<String>();
+		// add data to an array list to prepare to be input into trees 
 		while(inScanner.hasNext()){
 			cityName.add(inScanner.next());
 			cityPop.add(inScanner.next());
