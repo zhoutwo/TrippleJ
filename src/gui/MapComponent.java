2294fdae5a466cb2356642f2c31589ea5e96c5a8
Josh Woods <joshualwoods@gmail.com>
2016-02-10 14:29:18 -0500

    [Josh] I added empty classes for MapFrame and MapComponent. I also added the longitude and latitude data to the text file

---
+++ src/gui/MapComponent.java
@@ -0,0 +1,14 @@
+package gui;
+
+import javax.swing.JComponent;
+
+import backend.Map;
+
+public class MapComponent extends JComponent {
+	private Map stateMap;
+	
+	public MapComponent(){
+		
+	}
+
+}
