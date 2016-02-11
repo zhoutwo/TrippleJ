52400c27ba717d1d6d6dc3dc0090ebd02ce4d2d3
zhoutwo <7986384+zhoutwo@users.noreply.github.com>
2016-02-10 22:36:16 -0500

    [Joe] Updated POI.

--- src/backend/POI.java
+++ src/backend/POI.java
@@ -1,9 +1,26 @@
 package backend;
 
 public class POI extends Place {
+	
+	private String type;
+	private double cost;
 
-	public POI(String name, Coordinate location, int rating) {
+	public POI(String name, Coordinate location, String type, int rating, double cost) {
 		super(name, location, rating);
+		this.type = type;
+		this.cost = cost;
 	}
-
+	
+	public String getType() {
+		return this.type;
+	}
+	public double getCost() {
+		return this.cost;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public void setCost(double cost) {
+		this.cost = cost;
+	}
 }
