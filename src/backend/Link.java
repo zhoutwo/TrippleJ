3c2724ab1eb49f8fb1156f5a1d2a2d65f1a04c0f
Josh Woods <joshualwoods@gmail.com>
2016-02-18 18:07:40 -0500

    [Josh] fixed city naming problem with city methods and added time and type to links

--- src/backend/Link.java
+++ src/backend/Link.java
@@ -4,6 +4,9 @@ import utils.RoadType;
 
 public class Link {
 	
+	private static final int ISPEED = 85;
+	private static final int HSPEED = 45;
+	private static final int RSPEED = 40;
 	private int time;
 	private int distance;
 	private RoadType type;
@@ -14,8 +17,41 @@ public class Link {
 	public Link(String pName,Coordinate fromCor,Place pPlace){
 		place = pPlace;
 		name = pName;
+		setType();
+		setDistance();
+		setTime();
 	}
 	
+	private void setDistance() {
+		
+	}
+	
+	private void setTime() {
+		if(type.equals(RoadType.INTERSTATE)){
+			time = distance/ISPEED;
+		}
+		else if(type.equals(RoadType.HIGHWAY)){
+			time = distance/HSPEED;
+		}
+		else {
+			time = distance/RSPEED;
+		}
+	}
+
+	private void setType() {
+		char c = name.charAt(0);
+		String i = "Interstar";
+		if(c=='I'){
+			type = RoadType.INTERSTATE;
+		}
+		else if(c=='H'){
+			type = RoadType.HIGHWAY;
+		}
+		else {
+			type = RoadType.ROAD;
+		}
+	}
+
 	public int getTime() {
 		return this.time;
 	}
