3984be0bb51d782fb5334a25e1965cf583fa808d
zhoutwo <7986384+zhoutwo@users.noreply.github.com>
2016-02-21 15:06:43 -0500

    [Joe] More comments

--- src/backend/Place.java
+++ src/backend/Place.java
@@ -9,9 +9,9 @@ import java.util.HashMap;
  * location - location of the place
  * name - name of the place
  * neighbors - ArrayList of Links that connects to neighboring places
- * linkMap - !!!
+ * linkMap - A HashMap to retrieve the link to a specific Place
  * rating - rating of the place out of 5.0
- * est - !!!
+ * est - Calculations used by the cost function
  */
 public abstract class Place {
 	private Point centerPoint;
@@ -22,7 +22,7 @@ public abstract class Place {
 	protected double rating;
 	protected HashMap<Place,Double> est;
 	/**
-	 * !!!
+	 * 
 	 * @param name name of the place in String
 	 * @param location location of the place in Coordinate class
 	 * @param rating rating of place in double out of 5.0
@@ -42,25 +42,26 @@ public abstract class Place {
 	public boolean equals(Object obj) {
 		return (name.compareTo(((Place) obj).name) == 0);
 	}
+	
 	/**
-	 * !!!
-	 * @param p
-	 * @return
+	 * For use by Map when calculating routes
+	 * 
 	 */
 	public double getDEst(Place p){
 		return est.get(p);
 	}
+	
 	/**
-	 * !!!
-	 * @param p
-	 * @return
+	 * For use by Map when calculating routes
+	 * 
 	 */
 	public double getTEst(Place p){
 		return est.get(p)/85.0;
 	}
+
 	/**
-	 * !!!
-	 * @param al
+	 * For use by Map when calculating routes
+	 * 
 	 */
 	public void fillEstTable(ArrayList<Place> al){
 		Place temp;
@@ -88,14 +89,15 @@ public abstract class Place {
 		double yDistance = yFrom - yTo;
 		return Math.sqrt((xDistance*xDistance)+(yDistance*yDistance));
 	}
+
 	/**
-	 * !!!
-	 * @param p
-	 * @return
+	 * For use by Map when calculating routes
+	 * 
 	 */
 	public Double getEst(Place p){
 		return est.get(p);
 	}
+	
 	/**
 	 * initialize the center of its location drawn in the MapDisplayPanel
 	 */
@@ -105,6 +107,7 @@ public abstract class Place {
 		double x = (location.getX()+98)*(150);
 		centerPoint.setLocation(x,y);
 	}
+	
 	/**
 	 * return the center point of its location drawn in the MapDisplayPanel
 	 * @return
@@ -112,34 +115,39 @@ public abstract class Place {
 	public Point getMapLoc(){
 		return centerPoint;
 	}
+	
 	/**
 	 * return the GPS coordinate of the place
-	 * @return
+	 * @return the coordinates of the place
 	 */
 	public Coordinate getLocation() {
 		return this.location;
 	}
+	
 	/**
 	 * return the name of the place
-	 * @return
+	 * @return the name of the place
 	 */
 	public String getName() {
 		return this.name;
 	}
+	
 	/**
 	 * return the neighboring links of the place
-	 * @return
+	 * @return the ArrarList of links
 	 */
 	public ArrayList<Link> getNeighbors() {
 		return this.neighbors;
 	}
+	
 	/**
 	 * return the rating of the place
-	 * @return
+	 * @return the rating of the place
 	 */
 	public double getRating() {
 		return this.rating;
 	}
+	
 	/**
 	 * adds a neighboring link to the place taken from a parameter 
 	 * @param n
